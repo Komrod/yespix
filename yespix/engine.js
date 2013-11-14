178e7f13e5a27349cbdba8aeb95ab76127659599
komrod <komrod@hotmail.com>
2013-11-14 12:49:34 +0100

    key manager final

--- yespix/engine.js
+++ yespix/engine.js
@@ -2055,24 +2055,51 @@
 		 * INPUT KEYBOARD AND MOUSE
 		 */
 
+
+		/**
+		 * Returns True if some keys are pressed, hold, down or up for this frame. Note: the key arrays are reset every frame, only the "hold"
+		 * keys are kept until "keyup" event. Depending on the framerate, you will sometimes miss the "pressed", "up" and "down" event. If you 
+		 * want to trigger an event on a key, it's better to use yespix.on('keypress'), yespix.on('keydown') or yespix.on('keyup'). The operators 
+		 * AND "-" and OR "|" can be used in the selector.
+		 * @param  {int|string} s The selector or the key code of the character. Selector can be special keys ("shift", "ctrl" ...), multiple keys separated
+		 *                        with operator AND "-" ("ctrl-a", "a-d-g") or operator OR "|" ("a|2", "g|h|j"). Operator AND "-" have the priority 
+		 *                        over "|", meaning "a|b-c" will be parsed like "a" || ("b" && "c"). If looking for character "|" and "-", the character
+		 *                        must be escaped if there is more than one character in the selector, like "\|" and "\-". 
+		 * @param  {string} type "pressed" / "hold" / "down" / "up", default is "hold"
+		 * @return {boolean} Returns True on success
+		 * @example key("w") return true if the keys "w" is hold
+		 * @example key("ctrl-d") return true if the keys "control" and "d" are hold together
+		 * @example key("a-z-e-r") return true if the keys "a", "z", "e" and "r" are hold together
+		 * @example key("a|r") return true if the keys "a" OR "r" are hold
+		 * @example key("a", "up") return true if the key "a" is up for this frame
+		 * @example key("|", "pressed") return true if the key "|" is pressed for this frame
+		 * @example key("\-|\|") return true if the key "-" or "|" is hold
+		 * @example key("a|z-e|r") return true if the keys "a" || ("z" && "e") || "r" are hold
+		 * @example key("a-z|e-r") return true if the keys ("a" || "z") && ("e" || "r") are hold
+		 */
 		key: function(s, type) {
 			type = type || 'hold';
-//console.log('type = '+type);
+			//console.log('type = '+type);
 			if (this.isString(s)) {
-				if (s.indexOf('-') != -1) return this.key(s.split('-'), type);
+				if (s.indexOf('|') != -1 && s.charAt(s.indexOf('|')-1)!='\\' && s.length>1)
+				{
+					var arr = s.split('|', 2);
+					for (var t=0; t<arr.length; t++) if (this.key(arr[t], type)) return true;
+					return false;
+				}
+				if (s.indexOf('-') != -1 && s.charAt(s.indexOf('-')-1)!='\\' && s.length>1) return this.key(s.split('-', 2), type);
 				if (s.length > 1) return this.specialKey(s, type);
-
-				if (type == 'hold' && this.data.key['up'][s]) return true;
+				if (type != 'pressed') s = s.toUpperCase();
+				if (type == 'hold' && this.data.key['up'][s.charCodeAt(0)]) return true;
 				return !!this.data.key[type][s.charCodeAt(0)];
 			}
 
 			if (this.isArray(s)) {
 				for (var t = 0; t < s.length; t++)
-					if (!this.key(s[t])) return false;
+					if (!this.key(s[t], type)) return false;
 				return true;
 			}
-			if (this.isInt(s))
-			{
+			if (this.isInt(s)) {
 				if (type == 'hold' && this.data.key['up'][s]) return true;
 				return !!this.data.key[type][s];
 			}
@@ -2081,8 +2108,8 @@
 
 		specialKey: function(s, type) {
 			type = type || 'hold';
-			if (type == 'hold' && this.data.key['up'][this.data.key.special[s]]) return true;
-			return !!this.data.key[type][this.data.key.special[s]];
+			if (type == 'hold' && this.data.key['up'][this.data.key.special[s.toLowerCase()]]) return true;
+			return !!this.data.key[type][this.data.key.special[s.toLowerCase()]];
 		},
 
 	};
@@ -2895,11 +2922,15 @@
 
 			// special key pressed
 			if (e.ctrlKey) yespix.data.key.down[yespix.data.key.special['ctrl']] = true;
+			else yespix.data.key.hold[yespix.data.key.special['ctrl']] = false;
 			if (e.altKey) yespix.data.key.down[yespix.data.key.special['alt']] = true;
+			else yespix.data.key.hold[yespix.data.key.special['alt']] = false;
 			if (e.shiftKey) yespix.data.key.down[yespix.data.key.special['shift']] = true;
+			else yespix.data.key.hold[yespix.data.key.special['shift']] = false;
 
 			// triggers on YESPIX engine
 			yespix.trigger(e.type, e);
+			//return false;
 		};
 
 		/**
@@ -2919,16 +2950,20 @@
 
 			// special key pressed
 			if (e.ctrlKey) yespix.data.key.pressed[yespix.data.key.special['ctrl']] = true;
+			else yespix.data.key.hold[yespix.data.key.special['ctrl']] = false;
 			if (e.altKey) yespix.data.key.pressed[yespix.data.key.special['alt']] = true;
+			else yespix.data.key.hold[yespix.data.key.special['alt']] = false;
 			if (e.shiftKey) yespix.data.key.pressed[yespix.data.key.special['shift']] = true;
+			else yespix.data.key.hold[yespix.data.key.special['shift']] = false;
 
 			// triggers on YESPIX engine
 			yespix.trigger(e.type, e);
+			//return false;
 		};
 
 		/**
 		 * Key pressed bindings
-		 * 
+		 *
 		 * @param  {object} e Event
 		 */
 		document.onkeypress = function(e) {
@@ -2940,15 +2975,10 @@
 
 			// main key pressed
 			yespix.data.key.pressed[parseInt(e.code)] = true;
-			yespix.data.key.hold[parseInt(e.code)] = true;
-
-			// special key pressed
-			if (e.ctrlKey) yespix.data.key.pressed[yespix.data.key.special['ctrl']] = true;
-			if (e.altKey) yespix.data.key.pressed[yespix.data.key.special['alt']] = true;
-			if (e.shiftKey) yespix.data.key.pressed[yespix.data.key.special['shift']] = true;
 
 			// triggers on YESPIX engine
 			yespix.trigger(e.type, e);
+			//return false;
 		};
 
 		/**
